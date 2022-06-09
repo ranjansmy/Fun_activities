81d3085cd14fc6f09e74c473a73a4fa9069f4588
Ranjan Soumya <83495544+ranjansmy@users.noreply.github.com>
2022-06-09 08:25:28 +0530

    Reverse string
    Different ways to reverse a string

--- IR_reverse.py
+++ IR_reverse.py
@@ -1,5 +1,5 @@
 message = "This is a meaningful sentence"
-"""
+""" Method 1
 translated = ''
 
 i = len(message) - 1
@@ -9,9 +9,17 @@ while i >= 0:
 
 print(translated)
 """
-
+""" Method 2
 def reverse(string):
     string = string[::-1]
     return string
 
 print (reverse(message))
+
+"""
+# Method 3
+def reverse(string):
+    string = "".join(reversed(string))
+    return string
+
+print(reverse(message))
