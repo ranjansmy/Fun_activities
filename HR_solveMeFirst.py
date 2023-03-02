29f2cf7c1897a2b16121dff227ccbf40d48834e7
Ranjan Soumya <83495544+ranjansmy@users.noreply.github.com>
2023-03-02 23:25:26 +0530

    HR Solve Me First

---
+++ HR_solveMeFirst.py
@@ -0,0 +1,9 @@
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return(int(a + b))
+   #print(int(a + b)) This is not the correct answer, as the output should be only the sum, not sum and 'None'
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
